cfc53aff73fbf725e3abf2a54331b55c1305e014
gaesyeah <gabrielgoesrios@gmail.com>
2026-06-23 15:14:44 -0300

    chore: configure typeORM and sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      //only when developing, in that case we're using sqlite
+      synchronize: true,
+    }),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
