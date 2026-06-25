eafb31500434e68d70375603f2394f0438424b40
gaesyeah <gabrielgoesrios@gmail.com>
2026-06-24 23:33:24 -0300

    chore: synchronize false

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,7 @@ import { ConfigModule } from '@nestjs/config';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: false,
       ssl: {
         rejectUnauthorized: false,
       },
